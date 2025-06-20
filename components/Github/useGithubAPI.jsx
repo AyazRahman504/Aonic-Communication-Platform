4ae32efa68b6f70f2b2fe9311672bdbc034b594b
Ayaz Rahman Bhuiyan <81004595+AyazRahman504@users.noreply.github.com>
2025-06-20 16:46:33 +0800

    Add files via upload

---
+++ components/Github/useGithubAPI.jsx
@@ -0,0 +1,11 @@
+import { Octokit } from "octokit";
+
+/*const useGithubAPI = () => {
+  const octokit = new Octokit({
+    auth: "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",
+  });*/
+
+  return octokit;
+};
+
+export default useGithubAPI;
